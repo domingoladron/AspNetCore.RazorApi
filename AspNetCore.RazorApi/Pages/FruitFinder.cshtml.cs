c9814a6fc53988c30711f09ba4abf911b1689ca8
Chris <chris@coactive.io>
2019-11-03 14:13:40 +1300

    Basic empty FruitFinder cshtml and page model

---
+++ AspNetCore.RazorApi/Pages/FruitFinder.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AspNetCore.RazorApi.Pages
+{
+    public class FruitFinderModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
