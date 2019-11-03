4a9719cbaabb6f17ec0706f97fae19ffbd6b236e
Chris <chris@coactive.io>
2019-11-03 16:37:19 +1300

    Adding mock data and its return to our FruitFinder Razor page

--- AspNetCore.RazorApi/Pages/FruitFinder.cshtml.cs
+++ AspNetCore.RazorApi/Pages/FruitFinder.cshtml.cs
@@ -1,12 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace AspNetCore.RazorApi.Pages
 {
     public class FruitFinderModel : PageModel
     {
-        public void OnGet()
-        {
+        private static readonly List<string> Fruits = new List<string>();
 
+        static FruitFinderModel()
+        {
+            //Mocking out some data being returned from a call
+            Fruits.Add("apple");
+            Fruits.Add("avocado");
+            Fruits.Add("banana");
+            Fruits.Add("cantaloupe");
+            Fruits.Add("cherimoya");
+            Fruits.Add("feijoa");
+            Fruits.Add("grapefruit");
+            Fruits.Add("kumquat");
+            Fruits.Add("mango");
+            Fruits.Add("orange");
+        }
+
+        public JsonResult OnGet()
+        {
+            var randomIndex = new Random().Next(Fruits.Count - 1);
+            return new JsonResult(Fruits[randomIndex]);
         }
     }
 }
